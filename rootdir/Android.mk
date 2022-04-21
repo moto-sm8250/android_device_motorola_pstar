bffd362dbea830a09a06ba7868fc4008aba5251b
electimon <electimon@gmail.com>
2022-04-21 08:23:58 +0000

    pstar: rootdir: Import custom fingerprint detection script
    Signed-off-by: electimon <electimon@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -140,14 +140,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.oem.fingerprint.overlay.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.oem.fingerprint.overlay.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.oem.fingerprint2.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
--- rootdir/etc/init.oem.fingerprint2.sh
+++ rootdir/etc/init.oem.fingerprint2.sh
@@ -1,112 +1,61 @@
 #!/vendor/bin/sh
-#
-# Start indicated fingerprint HAL service
-#
-# Copyright (c) 2019 Lenovo
-# All rights reserved.
-#
-# April 15, 2019  chengql2@lenovo.com  Initial version
-# December 2, 2019  chengql2  Store fps_id into persist fs
+# Determine fingerprint sensor
+if [ ! -f /mnt/vendor/persist/fps/vendor_id ];
+then
+fps_id="none"
+else
+fps_id=$(cat /mnt/vendor/persist/fps/vendor_id)
+fi
+# Set fingerprint vendor
+prop_persist_fps=persist.vendor.hardware.fingerprint
+# this property store FPS_STATUS_NONE or FPS_STATUS_OK
+# after start fingerprint hal service, the hal service will set this property.
+prop_fps_status=vendor.hw.fingerprint.status
 
-script_name=${0##*/}
-script_name=${script_name%.*}
-function notice {
-    log -t "$scriptname" -p i "$*"
+set_permissions() {
+    if [ "$fps_id" = "fpc" ]
+    then
+        chown system:system /sys/class/fingerprint/fpc1020/irq
+    else
+        chmod 0660 /dev/esfp0
+        chown system:system /dev/esfp0
+    fi
 }
 
-persist_fps_id=/mnt/vendor/persist/fps/vendor_id
-persist_fps_id2=/mnt/vendor/persist/fps/last_vendor_id
-MAX_TIMES=20
-
-if [ ! -f $persist_fps_id ]; then
-    notice "warn: no associated persist file found"
-    return -1
-fi
-FPS_VENDOR_NONE=none
-FPS_VENDOR_EGIS=egis
-FPS_VENDOR_FPC=fpc
-
-prop_fps_status=vendor.hw.fingerprint.status
-prop_persist_fps=persist.vendor.hardware.fingerprint
-
-FPS_STATUS_NONE=none
-FPS_STATUS_OK=ok
-
-fps_vendor2=$(cat $persist_fps_id2)
-if [ -z $fps_vendor2 ]; then
-    fps_vendor2=$FPS_VENDOR_NONE
-fi
-notice "FPS vendor (last): $fps_vendor2"
-fps_vendor=$(cat $persist_fps_id)
-if [ -z $fps_vendor ]; then
-    fps_vendor=$FPS_VENDOR_NONE
-fi
-notice "FPS vendor: $fps_vendor"
-
-if [ $fps_vendor == $FPS_STATUS_NONE ]; then
-    notice "warn: boot as the last FPS"
-    fps=$fps_vendor2
-else
-    fps=$fps_vendor
-fi
-
-for i in $(seq 1 2)
-do
-
-setprop $prop_fps_status $FPS_STATUS_NONE
-if [ $fps == $FPS_VENDOR_FPC ]; then
-    notice "start fps_hal"
-    start fps_hal
-else
-    notice "start ets_hal"
-    start ets_hal
-fi
-
-notice "wait for HAL finish ..."
-fps_status=$(getprop $prop_fps_status)
-for ii in $(seq 1 $MAX_TIMES)
-do
-#    notice "check fps vendor status: $fps_status"
-    if [ $fps_status != $FPS_STATUS_NONE ]; then
-        break
-    fi
-    sleep 0.2
-    fps_status=$(getprop $prop_fps_status)
-done
-notice "fingerprint HAL status: $fps_status"
-
-if [ $fps_status == $FPS_STATUS_OK ]; then
-    notice "HAL success"
-    setprop $prop_persist_fps $fps
-    if [ $fps_vendor2 == $fps ]; then
-        return 0
-    fi
-    notice "- update FPS vendor (last)"
-    echo $fps > $persist_fps_id2
-    notice "- done"
-    return 0
-fi
-
-if [ $fps == $fps_vendor2 ]; then
-    if [ $fps == $FPS_VENDOR_FPC ]; then
-        rmmod fpc1020_mmi
-        insmod /vendor/lib/modules/ets_fps_mmi.ko
-        fps=$FPS_VENDOR_EGIS
-    else
-        rmmod ets_fps_mmi
+start_fpsensor() {
+    if [ "$fps_id" = "fpc" ]
+    then
         insmod /vendor/lib/modules/fpc1020_mmi.ko
-        fps=$FPS_VENDOR_FPC
+        sleep 0.6
+        set_permissions
+        sleep 0.4
+        start fps_hal
+        sleep 1
+        value=$(getprop "$prop_fps_status")
+        if [ "$value" = "ok" ];
+        then
+            setprop "$prop_persist_fps" "$fps_id"
+        fi
+    else
+        insmod /vendor/lib/modules/ets_fps_mmi.ko
+        sleep 0.6
+        set_permissions
+        sleep 0.4
+        start ets_hal
+        sleep 1
+        value=$(getprop "$prop_fps_status")
+        if [ "$value" = "ok" ];
+        then
+            setprop "$prop_persist_fps" "$fps_id"
+        fi
     fi
-    notice "- update FPS vendor"
-    echo $fps > $persist_fps_id
-    sleep 1
-else
-    notice "error: HAL fail"
-    setprop $prop_persist_fps $FPS_VENDOR_NONE
-    echo $FPS_VENDOR_NONE > $persist_fps_id
+}
 
-    notice "- done"
-    return 1
+rmmod ets_fps_mmi
+rmmod fpc1020_mmi
+sleep 0.5
+if [ "$fps_id" = "none" ];
+then
+    fps_id=$(cat /mnt/vendor/persist/fps/last_vendor_id)
 fi
-
-done
+start_fpsensor
